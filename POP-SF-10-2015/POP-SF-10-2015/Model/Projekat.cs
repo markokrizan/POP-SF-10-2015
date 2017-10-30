7c406c716a5ae9fd7fc21e76e330f7969ebae94b
markokrizan <32802857+markokrizan@users.noreply.github.com>
2017-10-30 10:12:34 +0100

    Vezbe 2

---
+++ POP-SF-10-2015/POP-SF-10-2015/Model/Projekat.cs
@@ -0,0 +1,55 @@
+﻿using POP_SF_10_2015.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POP_SF_10_2015.Model
+{
+    public class Projekat
+    {
+        //ZA singleton design pattern
+        //jedan objekat koji sadrzi sve ostale
+        //svaki put kad pristupis propertiju npr namestaja on ce da ucita iz fajla zbog ponasanja u geteru
+
+        //samo setter da niko ne moze da ga menja
+        //odma instanciramo
+        public static Projekat Instance { get;  } = new Projekat();
+
+        
+        private List<Namestaj> namestaj = new List<Namestaj>();
+
+        public  List<Namestaj> Namestaj
+        {
+            get {
+                //atribut namestaj(lista) ce biti ono sto procitas
+                this.namestaj = GenericSerializer.Deserialize<Namestaj>("namestaj.xml");
+                return this.namestaj;
+            }
+            set {
+                //uzmi sad to
+                this.namestaj = value;
+                GenericSerializer.Serialize<Namestaj>("namestaj.xml", this.namestaj);
+            }
+        }
+
+
+        /*
+        public List<Korisnik> Korisnici { get; set; }
+        public List<DodatneUsluge> DodatneUsluge { get; set; }
+        public List<Akcija> Akcije { get; set; }
+        */
+
+        public Projekat()
+        {
+            
+
+        }
+
+
+
+
+
+
+
+    }
+}
--- POP-SF-10-2015/POP-SF-10-2015/Program.cs
+++ POP-SF-10-2015/POP-SF-10-2015/Program.cs
@@ -1,4 +1,6 @@
-﻿using POP_SF_10_2015.Tests;
+﻿using POP_SF_10_2015.Model;
+using POP_SF_10_2015.Tests;
+using POP_SF_10_2015.Util;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,7 +13,93 @@ namespace POP_SF_10_2015
         static void Main(string[] args)
 
         {
-            KonzolnaAplikacija ka = new KonzolnaAplikacija();
+            //KonzolnaAplikacija ka = new KonzolnaAplikacija();
+
+            /*
+
+            var tn1 = new TipNamestaja()
+            {
+                ID = 1,
+                Naziv = "Podna lamba"
+            };
+
+            var n1 = new Namestaj()
+            {
+                ID = 1,
+                Naziv = "Svetlo",
+                Cena = 28,
+                KolicinaUMagacinu = 3,
+                Sifra = "234234234",
+                TipNamestaja = tn1
+            };
+
+            var n2 = new Namestaj()
+            {
+                ID = 1,
+                Naziv = "Lampurina",
+                Cena = 20,
+                KolicinaUMagacinu = 2,
+                Sifra = "23423425645",
+                TipNamestaja = tn1
+            };
+
+            var n3 = new Namestaj()
+            {
+                ID = 2,
+                Naziv = "Lampursdfsina",
+                Cena = 220,
+                KolicinaUMagacinu = 32,
+                Sifra = "234233425645",
+                TipNamestaja = tn1
+            };
+
+            var ln1 = new List<Namestaj>();
+            ln1.Add(n1);
+            ln1.Add(n2);
+            ln1.Add(n3);
+
+
+            //citam - izmenim - sacuvam // serialize - pisi deserialize - citaj
+            Console.WriteLine("Serialization...");
+
+            //uctiaj iz liste u fajl
+            GenericSerializer.Serialize<Namestaj>("namestaj.xml", ln1);
+
+            var ln2 = GenericSerializer.Deserialize<Namestaj>("namestaj.xml");
+
+            Console.WriteLine(ln2);
+
+            Console.WriteLine("Finished Serialization...");
+            Console.ReadLine();
+            */
+
+            List<Namestaj> namestaj = Projekat.Instance.Namestaj;
+
+
+
+            //dodavanje:
+
+            namestaj.Add(new Namestaj() { ID = 1, Naziv = "Novi namestaj" });
+            Projekat.Instance.Namestaj = namestaj;
+                       
+
+            //uradi ostale, pretragu, logovanje
+
+
+
+
+
+
+
+
+            foreach(var stavka in namestaj)
+            {
+                Console.WriteLine($"{stavka.Naziv}");
+            }
+            Console.ReadLine();
+
+
+
 
 
         }
